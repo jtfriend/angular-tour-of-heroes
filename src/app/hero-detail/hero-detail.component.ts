40a369b05f71bda7f0660f0ac14348a8af18e8d5
DESKTOP-EDNCCGL\Joe <jt2friend@gmail.com>
2021-02-10 19:56:14 +0000

    angular thinks something will be null, so is erroring

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,11 +15,28 @@ import { Hero } from '../hero';
 
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero | undefined;
+  hero: Hero | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    var id = 0;
+    if (+this.route.snapshot.paramMap.get('id') === null) {
+      id = 0;
+    } else {
+      id = +this.route.snapshot.paramMap.get('id');
+    }
+    
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
